df1808fcfd6529e8a139ec500487a4442c9af722
simonireilly@gmail.com <simonireilly@gmail.com>
2020-02-09 15:20:42 +0000

    Copy to macro system

--- server/linux_x11/plugins/recognition_results/config.py
+++ server/linux_x11/plugins/recognition_results/config.py
@@ -1,4 +1,3 @@
-
 """configuration for the plug-in and UI """
 
 import os
@@ -23,13 +22,13 @@ failure_literal = "???"
 # include a button to quit the application
 include_quit_button = False
 
-#appearance of the results
+# appearance of the results
 foreground_color = "black"
 background_color = "#e6e195"
 font_size = 12
-font_family = "Fira Sans" # will resort to default if font not found
+font_family = "Fira Sans"  # will resort to default if font not found
 
 # window type: see https://www.tcl-lang.org/man/tcl/TkCmd/wm.htm#M19
-window_type = 'dock' 
+window_type = "dock"
 # window should be always on top of other applications
 always_on_top = True
